5d57f2a69a4b73734539aeaab8e0d93017b61f9e
codyh <4218016+chodyo@users.noreply.github.com>
2019-04-28 17:13:24 -0400

    Restructure go code

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package cmd
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/chodyo/hafenhause"
+)
+
+func main() {
+	http.HandleFunc("/Bedtime", hafenhause.Bedtime)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/chodyo/hafenhause
+
+require (
+	github.com/codemodus/parth/v2 v2.0.1
+	github.com/mitchellh/mapstructure v1.1.2
+)
