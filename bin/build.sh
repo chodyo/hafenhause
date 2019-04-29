2f07c1f6ad01e75d77613eeb6856d438562d58b6
codyh <4218016+chodyo@users.noreply.github.com>
2019-04-28 21:38:44 -0400

    Use what seems to be the best firestore image

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,3 @@
 go mod tidy
 
 docker build -t hafenhause .
-
-docker-compose up -d --force-recreate
-
-docker-compose logs -f
---
+++ bin/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker-compose up -d --force-recreate
+
+docker-compose logs -f
